3b08a229216094066c8bf437b75d20ca9653f427
Peter Wagner <peter.wagner@shopify.com>
2024-12-12 13:25:43 -0500

    document the magic

--- lib/version.rb
+++ lib/version.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 module Meow
+  # This file is automatically modified by the release workflow.
+  # Changes will be ignored, and if you view a tag it is lying.
   VERSION = '0.1.0'
 end
